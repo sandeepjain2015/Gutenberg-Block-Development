07d2b5e4be1b5898b7128f97c6db9a83ab0fb424
sandeep jain <mr.sandeepmcscet@gmail.com>
2024-06-29 12:11:10 +0530

    first commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '743533ce94f0a11a6ee1');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '28f7e461eb9ab869c079');
